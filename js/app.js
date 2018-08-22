78c330c2f05ad06fa3432cc99007fdfda2cb47af
Bruno Arribas <36543599+brunoarr@users.noreply.github.com>
2018-08-22 22:10:03 +0200

    upload portfolio

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function viewMore(i) {
+    var x = document.getElementsByClassName("viewmore")[i];
+    var y = document.getElementsByClassName("viewmore-button")[i];
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        // x.style.opacity = 100;
+        // x.style.transition = 'opacity 1s ease-out';
+        y.innerHTML="View less ⮝";
+    } else {
+        x.style.display = "none";
+        y.innerHTML="View more ⮟";
+    }
+};
+
+$(".viewmore-button").click(function() {
+   var i = $(".viewmore-button").index(this);
+   viewMore(i);
+});
