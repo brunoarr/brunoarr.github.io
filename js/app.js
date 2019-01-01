dd89e6820a25d1c70fed85899404b2da8605ba9e
Bruno Arribas <36543599+brunoarr@users.noreply.github.com>
2019-01-01 18:47:58 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
+// function viewMore(i) {
+//     var x = document.getElementsByClassName("viewmore")[i];
+//     var y = document.getElementsByClassName("viewmore-button")[i];
+//
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//       y.innerHTML="View more +";
+//     } else {
+//       x.style.display = "block";
+//       y.innerHTML="View less -";
+//     }
+// };
+//
+// $(".viewmore-button").click(function() {
+//    var i = $(".viewmore-button").index(this);
+//    viewMore(i);
+// });
+
+
 function viewMore(i) {
+    //close all view mores
+    //for (a = 0; a < 6; a++) {
+      // document.getElementsByClassName("viewmore")[a].style.display = "none";
+      // document.getElementsByClassName("viewmore-button")[a].innerHTML="View more +";
+    //}
+    //change only the selected view more
     var x = document.getElementsByClassName("viewmore")[i];
     var y = document.getElementsByClassName("viewmore-button")[i];
 
